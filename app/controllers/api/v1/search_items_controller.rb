a88d749da5b672e3efe1302940984b1381413931
Daniel Rodriguez <drod2793@gmail.com>
2017-01-24 05:26:34 -0700

    Adds find item if updated_at is given as parameter

--- app/controllers/api/v1/search_items_controller.rb
+++ app/controllers/api/v1/search_items_controller.rb
@@ -10,6 +10,8 @@ class Api::V1::SearchItemsController < ApplicationController
       render json: Item.find_by(unit_price: params[:unit_price])
     elsif params[:created_at]
       render json: Item.find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      render json: Item.find_by(updated_at: params[:updated_at])
     end
   end
 end
