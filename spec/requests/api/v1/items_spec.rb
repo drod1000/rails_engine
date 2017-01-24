3f6f33fe51f6d6ba4e57a416226c62fb01c51074
Daniel Rodriguez <drod2793@gmail.com>
2017-01-23 19:41:21 -0700

    Adds test for list of all items

---
+++ spec/requests/api/v1/items_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "returns a list of all items" do
+    create_list(:items, 3)
+
+    get '/api/v1/items'
+
+    items = JSON.parse
+    item = items.first
+
+    expect(response).to be_success
+    expect(items.count).to eq(3)
+    expect(item).to be_a(Hash)
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item).to have_key("description")
+    expect(item).to have_key("unit_price")
+    expect(item).to have_key("merchant_id")
+    expect(item).to have_key("created_ath")
+    expect(item).to have_key("updated_at")
+  end
+end
