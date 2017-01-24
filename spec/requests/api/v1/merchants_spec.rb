8c37a44395dc07d3b56a02d7b1401daa2b92ab7f
Ann Wolff <annwolffe@gmail.com>
2017-01-24 09:21:37 -0700

    Remove skip on merchants tests

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -47,7 +47,7 @@ describe 'Merchants API' do
     expect(found_merchant["name"]).to eq(merchant.name)
   end
 
-  xit "can find a single record by created_at" do
+  it "can find a single record by created_at" do
     merchant = create(:merchant)
     get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
     found_merchant = JSON.parse(response.body)
@@ -57,7 +57,7 @@ describe 'Merchants API' do
     expect(found_merchant["name"]).to eq(merchant.name)
   end
 
-  xit "can find a single record by updated_at" do
+  it "can find a single record by updated_at" do
     merchant = create(:merchant)
     get "/api/v1/merchants/find?updated_at=#{merchant.updated_at}"
     found_merchant = JSON.parse(response.body)
@@ -65,10 +65,6 @@ describe 'Merchants API' do
     expect(response).to be_success
     expect(found_merchant["id"]).to eq(merchant.id)
     expect(found_merchant["name"]).to eq(merchant.name)
-  end
-
-  it "will return not find if params don't match" do
-    expect { get '/api/v1/merchants/find?foo=bar' }.to raise_error
   end
 
   it "can find all records matching id" do
@@ -94,7 +90,7 @@ describe 'Merchants API' do
     expect(found_merchants.first["name"]).to eq(Merchant.first.name)
   end
 
-  xit "can find all records matching created_at" do
+  it "can find all records matching created_at" do
     create_list(:merchant, 3)
     get "/api/v1/merchants/find_all?created_at=#{Merchant.first.created_at}"
     found_merchants = JSON.parse(response.body)
@@ -106,7 +102,7 @@ describe 'Merchants API' do
     expect(found_merchants.first["name"]).to eq(Merchant.first.name)
   end
 
-  xit "can find all records matching updated_at" do
+  it "can find all records matching updated_at" do
     create_list(:merchant, 3)
     get "/api/v1/merchants/find_all?updated_at=#{Merchant.first.updated_at}"
     found_merchants = JSON.parse(response.body)
@@ -116,10 +112,6 @@ describe 'Merchants API' do
     expect(found_merchants.count).to eq(1)
     expect(found_merchants.first["id"]).to eq(Merchant.first.id)
     expect(found_merchants.first["name"]).to eq(Merchant.first.name)
-  end
-
-  it "will return not find_all if params don't match" do
-    expect { get '/api/v1/merchants/find_all?foo=bar' }.to raise_error
   end
 
   it "can find a random merchant" do
