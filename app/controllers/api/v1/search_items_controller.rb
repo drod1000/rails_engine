94489287a9ec3197423dab70aebb451d09fc1fb0
Daniel Rodriguez <drod2793@gmail.com>
2017-01-23 20:57:28 -0700

    Adds find item if name given as parameter

--- app/controllers/api/v1/search_items_controller.rb
+++ app/controllers/api/v1/search_items_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SearchItemsController < ApplicationController
   def show
     if params[:id]
-       render json: Item.find(params[:id])
+       render json: Item.find_by(id: params[:id])
+    elsif params[:name]
+      render json: Item.find_by(name: params[:name])
+      
     end
   end
 end
