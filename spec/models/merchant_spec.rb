c6d804ca2f66e58e647592c4430adb42e78987fc
Daniel Rodriguez <drod2793@gmail.com>
2017-01-24 18:08:32 -0700

    Adds 1-many relationship for merchant-items and merchant-invoices

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it "can find a random merchant" do
-    create_list(:merchant, 3)
-    found_merchant = Merchant.random
+  describe "relationships" do
+    it "has many items" do
+      merchant = create(:merchant)
 
-    expect(Merchant.all.include?(found_merchant)).to be_truthy
-    expect(found_merchant).to be_a(Merchant)
+      expect(merchant).to respond_to(:items)
+    end
+
+    it "has many invoices" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:invoices)
+    end
   end
 end
