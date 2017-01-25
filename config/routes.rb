77793dbff783479abc61f6e382d4dac7debe4067
Daniel Rodriguez <drod2793@gmail.com>
2017-01-24 21:08:36 -0700

    Adds merchant-invoices endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       get '/merchants/find', to: 'search_merchants#show'
       get '/merchants/random', to: 'random_merchants#show'
       get '/merchants/:id/items', to: 'merchant_items#index'
+      get '/merchants/:id/invoices', to: 'merchant_invoices#index'
       resources :merchants, only: [:index, :show]
       get '/customers/find_all', to: 'search_customers#index'
       get '/customers/find', to: 'search_customers#show'
--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -161,4 +161,18 @@ describe 'Merchants API' do
     expect(items.count).to eq(3)
   end
 
+  it "can return associated invoices" do
+    merchant = create(:merchant)
+    create_list(:invoice, 4, merchant_id: merchant.id)
+    create_list(:invoice, 3)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    invoices = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoices).to be_a(Array)
+    expect(invoices.count).to eq(4)
+  end
+
 end
